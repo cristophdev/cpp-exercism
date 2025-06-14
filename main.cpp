e1f227ac67468cc027f868bc972a7e0f8b4937b1
Cristopher <cristoperesc@gmail.com>
2025-06-14 13:07:28 -0500

    hello world

--- main.cpp
+++ main.cpp
@@ -1,21 +1,6 @@
 #include <iostream>
-#include "src/lasagna.h"
-
-using namespace lasagna;
 
 int main() {
-  int time = 39;
-  int timeTwo = 20;
-  int timeThree = 30;
-
-  int ovenT = lasagna::ovenTime();
-  int remainingT = lasagna::remainingOvenTime(time);
-  int preparationT = lasagna::preparationTime(timeTwo);
-  int elapsedT = lasagna::elapsedTime(timeTwo, timeThree); 
-
-  std::cout << ovenT << std::endl;
-  std::cout << remainingT << std::endl;
-  std::cout << preparationT<< std::endl;
-  std::cout << elapsedT << std::endl;
+  std::cout << "HELLO, WORLD!" << std::endl;  
   return 0;
 }
