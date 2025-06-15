3d36080b0ea321745773b1980a67b33d45396e5e
Cristopher <cristoperesc@gmail.com>
2025-06-15 13:13:12 -0500

    power of troy

--- main.cpp
+++ main.cpp
@@ -1,21 +1,30 @@
 #include <iostream>
-#include "src/lasagna_master/lasagna_master.h"
+#include "src/power_of_troy/power_of_troy.h"
 
 using namespace std;
 
 int main()
 {
-  ingredients list {"Pim", "Pam", "Truku", "Truku"};
+  troy::human HUMAN1;
+  troy::human HUMAN2;
+  troy::give_new_artifact(HUMAN1, "MIGHT");
+  troy::give_new_artifact(HUMAN2, "WILL");
 
-  int time = lasagna_master::preparationTime(list, 2);
-  lasagna_master::addSecretIngredient(list, "mtc");
+  // type: troy::artifact *POSSESION_PT
+  auto POSSESION_PT = HUMAN1.possession.get();
+  auto POSSESION2_PT = HUMAN2.possession.get();
 
-  cout << "Preparation Time: " << time << endl;
+  cout << "Before exchange:" << endl;
+  cout << "HUMAN1 artifact: " << POSSESION_PT->name << " at " << POSSESION_PT << endl;
+  cout << "HUMAN2 artifact: " << POSSESION2_PT->name << " at " << POSSESION2_PT << endl;
 
-  cout << "Ingredients: " << endl;
-  for (auto i : list)
-  {
-    cout << i << endl;
-  }
+  troy::exchange_artifacts(HUMAN1.possession, HUMAN2.possession);
+
+  POSSESION_PT = HUMAN1.possession.get();
+  POSSESION2_PT = HUMAN2.possession.get();
+
+  cout << "\nAfter exchange:" << endl;
+  cout << "HUMAN1 artifact: " << POSSESION_PT->name << " at " << POSSESION_PT << endl;
+  cout << "HUMAN2 artifact: " << POSSESION2_PT->name << " at " << POSSESION2_PT << endl;
   return 0;
 }
