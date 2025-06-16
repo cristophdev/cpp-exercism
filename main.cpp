1256f0a46188212b685058051a66225cf8611325
Cristopher <cristoperesc@gmail.com>
2025-06-16 06:13:28 -0500

    space age

--- main.cpp
+++ main.cpp
@@ -1,12 +1,14 @@
 #include <iostream>
-#include "src/reverse_string/reverse_string.h"
+#include "src/space_age/space_age.h"
 
 using namespace std;
 
 int main()
 {
-  string name = "Cristopher";
+  space_age::space_age MY_AGE{631152000};
 
-  cout << "Original string: " << name << " reversed string: " << reverse_string::reverse_string(name) << endl;
+  cout << "On earth: " << MY_AGE.on_earth() << endl;
+  cout << "On Jupiter: " << MY_AGE.on_jupiter() << endl;
+  cout << "Mercury: " << MY_AGE.on_mercury() << endl;
   return 0;
 }
