a71fb9d9e54208b1c3a329d216a337c658bd45eb
Cristopher <cristoperesc@gmail.com>
2025-06-16 19:45:34 -0500

    triangle

--- main.cpp
+++ main.cpp
@@ -1,14 +1,25 @@
 #include <iostream>
-#include "src/space_age/space_age.h"
+#include "src/triangle/triangle.h"
 
 using namespace std;
 
+std::string to_string(triangle::flavor f) {
+  switch (f) {
+    case triangle::flavor::equilateral: return "equilateral";
+    case triangle::flavor::isosceles:   return "isosceles";
+    case triangle::flavor::scalene:     return "scalene";
+    default: return "unknown";
+  }
+}
+
 int main()
 {
-  space_age::space_age MY_AGE{631152000};
+  double A = 3.0;
+  double B = 3.0;
+  double C = 3.0;
 
-  cout << "On earth: " << MY_AGE.on_earth() << endl;
-  cout << "On Jupiter: " << MY_AGE.on_jupiter() << endl;
-  cout << "Mercury: " << MY_AGE.on_mercury() << endl;
+  auto w = triangle::kind(A, B, C);
+
+  cout << to_string(w) << endl;
   return 0;
 }
